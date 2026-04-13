ec7fd9095c48f3c3d37e0c5a04cf758038a5454c
Arshad-d8 <71703633+Arshad-d8@users.noreply.github.com>
2026-04-13 18:33:54 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,9 @@ export default function App() {
   return (
     <div>
       <Routes>
-       <Route path='/' element={<Home />} /> 
+        <Route path='/' element={<Home />} />
       </Routes>
-     
+
     </div>
   )
 }
