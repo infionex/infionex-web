9db2b6df133574ff48d34c04c54eac621d85719f
Mohammedshibili10 <shibiliek516@gmail.com>
2026-04-13 23:39:38 +0530

    components added

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import WorkSection from '../components/WorkSection'
+import ServicesSection from '../components/ServicesSection'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <WorkSection />
+      <ServicesSection/>
+    </div>
   )
 }
