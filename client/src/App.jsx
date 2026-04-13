b6c9f7a6f92f4bc4a2152ac92eff21a22651ee53
Mohammedshibili10 <shibiliek516@gmail.com>
2026-04-13 16:10:55 +0530

    routes install

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Routes } from 'react-router-dom'
+import Home from './page/Home'
 
 export default function App() {
   return (
     <div>
-      infionex
+      <Routes>
+       <Route path='/' element={<Home />} /> 
+      </Routes>
+     
     </div>
   )
 }
