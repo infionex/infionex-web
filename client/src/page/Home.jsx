16b25e426b739613cbaff8a5ae1f27d3eee6b766
shuhaib <shuhaibpmohammed@gmail.com>
2026-04-14 17:35:20 +0530

    process and contact section

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../components/home/Hero'
 import ServicesSection from '../components/ServicesSection'
 import WorkSection from '../components/WorkSection'
+import ProcessSection from '../components/ProcessSection'
+import ContactSection from '../components/ContactSection'
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
         <Hero/>
         <ServicesSection/>
         <WorkSection/>
+        <ProcessSection/>
+        <ContactSection/>
     </div>
   )
 }
